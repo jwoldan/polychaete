9b30116d80621b024bd14a3e16a24925db2c082a
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-21 17:00:05 -0500

    Create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/polychaete.js',
+  output: {
+    path: path.join(__dirname, 'javascripts'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  externals: {
+    "createjs": "createjs",
+  },
+};
