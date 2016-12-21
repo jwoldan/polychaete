31f8b63cbbf571915109891e0dc4c06726d4d73e
Jeffrey Woldan <jwoldan@gmail.com>
2016-12-21 17:00:26 -0500

    Create game sprites and implement test render

---
+++ frontend/polychaete.js
@@ -0,0 +1,50 @@
+import createjs from 'createjs';
+import {
+  createHeadSprite,
+  createSegmentSprite,
+  createSpongeSprite,
+  createDiverSprite
+} from './sprite_sheets';
+
+const frameRate = 9;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const stage = new createjs.Stage('canvas');
+
+  var shape = new createjs.Shape();
+  shape.graphics.beginFill('black').drawRect(0, 0, 500, 700);
+  stage.addChild(shape);
+  stage.update();
+
+  const head = createHeadSprite(frameRate);
+  const segment = createSegmentSprite(frameRate);
+  const sponge = createSpongeSprite(frameRate);
+  const diver = createDiverSprite(frameRate);
+
+  const headSprite = new createjs.Sprite(head, "moveUp");
+  stage.addChild(headSprite);
+
+  const segmentSprite = new createjs.Sprite(segment, "moveUp");
+  segmentSprite.x = 16;
+  stage.addChild(segmentSprite);
+
+  const spongeSpriteNew = new createjs.Sprite(sponge, "new");
+  spongeSpriteNew.x = 32;
+  stage.addChild(spongeSpriteNew);
+
+  const spongeSpriteOneHit = new createjs.Sprite(sponge, "oneHit");
+  spongeSpriteOneHit.x = 52;
+  stage.addChild(spongeSpriteOneHit);
+
+  const spongeSpriteTwoHits = new createjs.Sprite(sponge, "twoHits");
+  spongeSpriteTwoHits.x = 72;
+  stage.addChild(spongeSpriteTwoHits);
+
+  const diverSprite = new createjs.Sprite(diver, "default");
+  diverSprite.x = 90;
+  stage.addChild(diverSprite);
+
+  createjs.Ticker.on("tick", stage);
+
+});
---
+++ frontend/sprite_sheets.js
@@ -0,0 +1,88 @@
+import createjs from 'createjs';
+
+export const createDiverSprite = (frameRate) => (
+  new createjs.SpriteSheet({
+    frames: {
+      width: 20,
+      height: 20,
+    },
+    images: ['./assets/diver.png'],
+    animations: {
+      default: 0
+    }
+  })
+);
+
+export const createHeadSprite = (frameRate) => (
+  new createjs.SpriteSheet({
+    frames: [
+      // move left
+      [0, 0, 16, 20],
+      [16, 0, 16, 20],
+      [32, 0, 16, 20],
+      // move right
+      [0, 20, 16, 20],
+      [16, 20, 16, 20],
+      [32, 20, 16, 20],
+      // move down
+      [0, 40, 20, 20],
+      [20, 40, 20, 20],
+      [40, 40, 20, 20],
+      // move up
+      [0, 60, 20, 20],
+      [20, 60, 20, 20],
+      [40, 60, 20, 20],
+    ],
+    images: ['./assets/head.png'],
+    animations: {
+      moveLeft: [0, 2],
+      moveRight: [3, 5],
+      moveDown: [6, 8],
+      moveUp: [9, 11],
+    },
+    framerate: frameRate,
+  })
+);
+
+export const createSegmentSprite = (frameRate) => (
+  new createjs.SpriteSheet({
+    frames: [
+      // left/right
+      [0, 0, 16, 20],
+      [16, 0, 16, 20],
+      [32, 0, 16, 20],
+      // up/down
+      [0, 20, 20, 20],
+      [20, 20, 20, 20],
+      [40, 20, 20, 20],
+    ],
+    images: ['./assets/segment.png'],
+    animations: {
+      moveLeft: [0, 2],
+      moveDown: [3, 5],
+      moveRight: {
+        frames: [2, 1, 0],
+      },
+      moveUp: {
+        frames: [5, 4, 3],
+      },
+    },
+    framerate: frameRate,
+  })
+);
+
+export const createSpongeSprite = (frameRate) => (
+  new createjs.SpriteSheet({
+    frames: {
+      width: 20,
+      height: 20,
+    },
+    images: ['./assets/sea_sponge.png'],
+    animations: {
+      new: 0,
+      oneHit: 1,
+      twoHits: 2,
+    },
+    framerate: frameRate,
+  })
+);
